158128b1b0a005349788ffb016303fcfdaec6ac6
Maori Sayoud--Leclercq <maori_sayoud--leclercq@ens.univ-artois.fr>
2023-12-21 16:42:43 +0100

    resolution bug image histoire créer

--- app/Http/Controllers/HistoireController.php
+++ app/Http/Controllers/HistoireController.php
@@ -98,7 +98,7 @@ class HistoireController extends Controller
                 Storage::disk('public')->delete($newhistoire->photo);
             }
             $photo = $request->file('photo');
-            $path = $photo->store('images', 'public');
+            $path = $photo->store('storage/images', 'public');
             $newhistoire->photo = $path;
         }
         $parsedown = new Parsedown();
