86637082028a9a64bc33f60725cad61d75c99b8d
Maori Sayoud--Leclercq <maori_sayoud--leclercq@ens.univ-artois.fr>
2023-12-21 17:21:22 +0100

    bug vite.confi

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,7 @@ export default defineConfig({
   plugins: [
     laravel({
       input: [
-        "resources/css/normalize.css",
-        "resources/css/app.css",
-        "resources/js/app.js",
-        "resources/css/test-vite.css",
-        "resources/js/test-vite.js",
-        "resources/css/Accueil.css",
-        "resources/css/Equipes.css",
-        "resources/css/login.css",
-        "resources/css/catalogue.css",
-        "resources/css/show.css",
-        "resources/css/404.css",
-        "resources/css/create.css",
+        "resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js', 'resources/css/Accueil.css','resources/css/Equipes.css','resources/css/login.css','resources/css/catalogue.css','resources/css/show.css','resources/css/create.css','resources/css/profile.css','resources/css/FilmGenre.css','resources/css/edit.css'
       ],
       refresh: true,
     }),
